f4ca0ddce92a93e3220cebb2b3dbcdbf9ad92a86
Chris Pyles <cpyles017@gmail.com>
2025-04-24 22:47:00 -0500

    added an if else statement to find the difference of absolute value if the input number was above 21 and printed results

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,7 +2,16 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+    
+    if int(in_num) > 21:
+        diff = abs(2 * (int(in_num) - 21))
+
+    else: 
+        diff = abs(int(in_num) - 21)
+    
+    print("Result:", diff)
     # end assignment
+
 
 ## If you want to test locally run > python payCalculator.py
 
